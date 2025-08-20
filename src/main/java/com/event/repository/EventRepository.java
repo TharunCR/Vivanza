122fe15690abf97e776e513d50f31eb9a6461d78
tharuncr <crtharun20@gmail.com>
2025-08-20 21:57:49 +0530

    updated

--- src/main/java/com/event/repository/EventRepository.java
+++ src/main/java/com/event/repository/EventRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
-    //    Page<Event> findAll(Pageable pageable);
+    //   Page<Event> findAll(Pageable pageable);
     Optional<Event> findByIdAndUser(long id, User user);
 
     List<Event> findByUser(User user);
